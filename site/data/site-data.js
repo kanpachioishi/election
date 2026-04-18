c83472674ec0b84f0bbc0caeb3f513dba6c45caf
ryoto <43799059+kanpachioishi@users.noreply.github.com>
2026-04-19 00:46:34 +0900

    refactor: refine homepage notification card

--- site/data/site-data.js
+++ site/data/site-data.js
@@ -3,7 +3,7 @@
 // Update data/v1 and rerun the generator instead of editing this file.
 
 window.ELECTION_SITE_DATA = {
-  "generatedAt": "2026-04-18T15:40:10.341Z",
+  "generatedAt": "2026-04-18T15:46:34.668Z",
   "sourceGeneratedAt": "2026-04-16T02:40:00+09:00",
   "stats": {
     "regions": 201,
